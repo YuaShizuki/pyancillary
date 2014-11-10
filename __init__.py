004477e2bd001d10ac98484671308fdbbd0aabe3
YuaShizuki <sumodrop@gmail.com>
2014-11-09 21:09:25 -0800

    added pyancil

---
+++ __init__.py
@@ -0,0 +1,2 @@
+form pyancil import send_fd, recv_fd
+del pyancil
---
+++ pyancil.py
@@ -0,0 +1,26 @@
+import socket
+from cffi import FFI
+ffi = FFI()
+
+ffi.cdef("""
+    int ancil_send_fd(int, int);
+    int ancil_recv_fd(int, int *);
+""")
+
+lib = ffi.verify( """ #include "ancillary.h" """, 
+                include_dirs=[os.getcwd()],
+                sources=["fd_recv.c", "fd_send.c"])
+
+def sendfd(sock, fd):
+    return lib.ancil_send_fd(ffi.cast("int", sock.fileno()), ffi.cast("int", fd))
+
+
+# return's the socket fd. In case of error, returns -1
+def recvfd(sock):
+    recv_fd = cffi.new("int *")
+    recv_fd[0] = 0
+    ret = lib.ancil_recv_fd(ffi.cast("int", sock.fileno()), recv_fd)
+    if ret == 0:
+        return int(recv_fd)
+    else:
+        return ret 
