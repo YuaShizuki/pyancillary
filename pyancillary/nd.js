5ac6559c35e47d2b82c999c2dd5387e201f42d81
YuaShizuki <sumodrop@gmail.com>
2014-11-30 02:13:21 -0800

    wrk

--- pyancillary/nd.js
+++ pyancillary/nd.js
@@ -1,6 +1,17 @@
+var cluster = require('cluster');
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80, '127.0.0.1');
+var numCPUs = require('os').cpus().length;
 
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else { 
+    http.createServer(function(req, res) {
+        res.writeHead(200);
+        res.end("hello world\n");
+    }).listen(80);
+}
